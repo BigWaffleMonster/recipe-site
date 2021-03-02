a777ffde05afd24f464ba865193699b61c009a85
BigWaffleMonster <bigwafflemonster@gmail.com>
2021-03-02 11:49:19 +0300

    created setupProxy file and fixed auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,92 +1,93 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
-const config = require('config')
 const jwt = require('jsonwebtoken')
+const config = require('config')
 const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
-// /api/auth/register
 router.post(
   '/register',
   [
     check('email', 'Invalid email').isEmail(),
-    check('password', 'Minimun password lenght - 6').isLength({ min: 6 })
+    check('password', 'Minimum password length - 6 characters')
+      .isLength({ min: 6 })
   ],
   async (req, res) => {
   try {
     const errors = validationResult(req)
-    
+
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
-        message: 'Incorrect data'
+        message: 'Invalid data'
       })
     }
-    
+
     const {email, password} = req.body
-    
+
     const candidate = await User.findOne({ email })
-    
+
     if (candidate) {
-      return res.status(400).json({ message: 'Such user already exists!' })
+      return res.status(400).json({ message: 'User with such email already exists' })
     }
     
-    const hashedPassword = await bcrypt.hash(password, 121)
-    
+    const hashedPassword = await bcrypt.hash(password, 12)
     const user = new User({ email, password: hashedPassword })
-    
+
     await user.save()
-    
-    res.status(201).json({ message: 'User Created!' })
-    
-  } catch (e) {
-    res.status(500).json({ message: 'Something went wrong! Try again' })
+
+    return res.status(201).json({ message: 'User created' })
+
+
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong. Try again' })
   }
 })
 
-// api/auth/login
+
 router.post(
-  '/login',
+  '/login', 
   [
-    check('email', 'Invalid email').normalizeEmail().isEmail(),
-    check('password', 'Minimun password lenght - 6 ').exists().isLength({ min: 6 })
+    check('email', 'Invalid data').normalizeEmail().isEmail(),
+    check('password', 'Invalid password').exists()
   ],
   async (req, res) => {
   try {
     const errors = validationResult(req)
- 
+  
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
-        message: 'Incorrect data'
+        message: 'Invalid data'
       })
     }
-    
+  
     const {email, password} = req.body
-    
-    const user = User.findOne({ email })
-    
+  
+    const user = await User.findOne({ email })
+
     if (!user) {
-      return res.status(400).json({ message: 'Such user doesn`t exist! ' })
+      return res.status(400).json({ message: 'User is not found' })
     }
     
     const isMatch = await bcrypt.compare(password, user.password)
-    
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Incorrect data!' })
+
+    if(!isMatch) {
+      return res.status(400).json({ message: 'Invalid data' })
     }
-    
+
     const token = jwt.sign(
       { userId: user.id },
-      config.get('jwtSecret'),
-      { expiresIn: '1h' }
+      config.get('jwtSecrete'),
+      { expiresIn: '1h'}
     )
-    
+
     res.json({ token, userId: user.id })
-    
-  } catch (e) {
+
   
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong. Try again' })
   }
 })
 
