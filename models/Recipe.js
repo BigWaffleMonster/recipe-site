d4db3da26be582278cf1402254f4543f2cd4b153
BigWaffleMonster <bigwafflemonster@gmail.com>
2021-03-12 19:12:31 +0300

    just commit

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,11 @@
 const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  UserRecipes: [{type: Types.ObjectId, ref: 'Recipe'}]
+  title: {type: String, required: true},
+  body: {type: String, required: true},
+  image: {type: , required: true},
+  likes: {type: Number, default: 0},
+  comments: [{type: Types.ObjectId, ref: 'Comment'}]
 })
 
 module.exports = model('User', schema)
